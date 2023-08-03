e63864b93cf13320b520457f67d47d058d08d03e
Anand Mukund <am6191247@gmail.com>
2023-08-03 21:18:50 +0530

    Java class

--- Employee.java
+++ Employee.java
@@ -3,18 +3,9 @@ class Employee{
         static int marks;
         static String name;
         static String department;
-        static String stname()
-        {
-            return name;
-        }
-        static String depar()
-        {
-            return department;
-        }
-        static int stmarks()
-        {
-            return marks;
-        }
+       
+
+   
     }
     public static void main(String[] args) {
         Student.name="Anand";
